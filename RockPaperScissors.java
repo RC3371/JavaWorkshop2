ddd427769afac7609ab43463698d1eb47205a295
Ronak Chadha <57050464+RC3371@users.noreply.github.com>
2022-01-22 16:09:19 -0800

    Updated variable "CPU" to "cpu", added default case to switch statement

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -7,47 +7,49 @@ public class RockPaperScissors {
 		int losses = 0;
 		while(rounds <= 3) {
 			double CPUChoice = Math.random() * 10;
-			int CPU = (int) CPUChoice;
-			if(CPU == 9) {
-				while(CPU == 9) {
+			int cpu = (int) CPUChoice;
+			if(cpu == 9) {
+				while(cpu == 9) {
 					CPUChoice = Math.random() * 10;
-					CPU = (int) CPUChoice;
+					cpu = (int) CPUChoice;
 				}
 			}
-			switch(CPU) {
+			switch(cpu) {
 				case 1:
-					CPU = 0;
+					cpu = 0;
 					break;
 				case 2:
-					CPU = 0;
+					cpu = 0;
 					break;
 				case 3: 
-					CPU = 1;
+					cpu = 1;
 					break; 
 				case 4:
-					CPU = 1;
+					cpu = 1;
 					break;
 				case 5: 
-					CPU = 1;
+					cpu = 1;
 				case 6:
-					CPU = 2;
+					cpu = 2;
 					break;
 				case 7:
-					CPU = 2;
+					cpu = 2;
 					break;
 				case 8:
-					CPU = 2;
+					cpu = 2;
+					break;
+				default:
 					break;
 			}
 			System.out.println("Enter 0 to play rock, 1 to play paper, or 2 to play scissors");
 			int guess = myKeyboard.nextInt();
 			
-			if((guess == 0 && CPU == 2) || (guess == 1 && CPU == 0) || (guess == 2 && CPU == 1)) {
+			if((guess == 0 && cpu == 2) || (guess == 1 && cpu == 0) || (guess == 2 && cpu == 1)) {
 				wins++;
 				System.out.println("You have won! You have " + wins + " wins and " + losses + " losses");
 				rounds++;
 			}
-			else if(guess == CPU) {
+			else if(guess == cpu) {
 				System.out.println("You have tied! You have " + wins + " wins and " + losses + " losses");
 			}
 			else {
